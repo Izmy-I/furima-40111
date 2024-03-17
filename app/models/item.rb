2aa8d6e6963cd463cb2163e87616428ac3b8108b
Izmy-I <believe06again11@yahoo.co.jp>
2024-03-17 19:04:50 +0900

    コードレビュー修正①

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :fee_pay_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :image, presence: true
 
   belongs_to :user
